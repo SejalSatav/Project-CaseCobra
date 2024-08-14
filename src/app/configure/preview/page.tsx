753a78506416a567e58724b86ef9b91be575a59e
Kshiteej Jain <145803361+SejalSatav@users.noreply.github.com>
2024-08-14 18:24:15 -0400

    Public repository for CaseCobra project

---
+++ src/app/configure/preview/page.tsx
@@ -0,0 +1,32 @@
+import { db } from '@/db'
+import { notFound } from 'next/navigation'
+import DesignPreview from './DesignPreview'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+
+interface PageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined
+  }
+}
+
+const Page = async ({ searchParams }: PageProps) => {
+  const { getUser } = getKindeServerSession()
+  const user = await getUser()
+  const { id } = searchParams
+
+  if (!id || typeof id !== 'string') {
+    return notFound()
+  }
+
+  const configuration = await db.configuration.findUnique({
+    where: { id },
+  })
+
+  if (!configuration) {
+    return notFound()
+  }
+
+  return <DesignPreview configuration={configuration} user={user} />
+}
+
+export default Page
